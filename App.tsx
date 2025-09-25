78c0e152ce9574b952ef5a6a27a931a2cca04bf8
0xMarcio <marc@syntax9.ai>
2025-09-25 23:36:45 +0200

    feat(ui): add Marquee, NeonGrid (3D ground), and Holographic Showcase sections; deepen cinematic aesthetic

--- App.tsx
+++ App.tsx
@@ -1,5 +1,8 @@
 import Hero from './components/Hero'
 import Research from './components/Research'
+import Marquee from './components/Marquee'
+import NeonGrid from './components/NeonGrid'
+import Showcase from './components/Showcase'
 import Principles from './components/Principles'
 import Footer from './components/Footer'
 
@@ -8,6 +11,9 @@ export default function App() {
     <div className="dark min-h-screen bg-black text-white overflow-x-hidden relative">
       <Hero />
       <Research />
+      <Marquee />
+      <Showcase />
+      <NeonGrid />
       <Principles />
       <Footer />
     </div>
