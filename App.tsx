f805c1fddb0378ced596fbac4df81a6f0cdbdd2e
0xMarcio <marc@syntax9.ai>
2025-09-25 23:41:32 +0200

    polish: consistent spacing/visual rhythm; build verified

--- App.tsx
+++ App.tsx
@@ -1,19 +1,17 @@
+import Background from './components/Background'
 import Hero from './components/Hero'
+import Projects from './components/Projects'
 import Research from './components/Research'
-import Marquee from './components/Marquee'
-import NeonGrid from './components/NeonGrid'
-import Showcase from './components/Showcase'
 import Principles from './components/Principles'
 import Footer from './components/Footer'
 
 export default function App() {
   return (
     <div className="dark min-h-screen bg-black text-white overflow-x-hidden relative">
+      <Background />
       <Hero />
+      <Projects />
       <Research />
-      <Marquee />
-      <Showcase />
-      <NeonGrid />
       <Principles />
       <Footer />
     </div>
