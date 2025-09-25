89eb45d365f1c743d4fa18af64fb135d2768cfb2
0xMarcio <marc@syntax9.ai>
2025-09-25 23:27:23 +0200

    feat: weave in Research, Publications, and Updates sections

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
 import Hero from './components/Hero'
+import Research from './components/Research'
+import Publications from './components/Publications'
 import Features from './components/Features'
+import Updates from './components/Updates'
 import Footer from './components/Footer'
 
 export default function App() {
   return (
     <div className="dark min-h-screen bg-black text-white overflow-x-hidden relative">
       <Hero />
+      <Research />
+      <Publications />
       <Features />
+      <Updates />
       <Footer />
     </div>
   )
